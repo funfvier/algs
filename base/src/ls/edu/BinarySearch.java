6d19c6a0043d80db5863ab9a07a6fc0eea8bef9a
lshi <l.shitov@mangotele.com>
2018-08-23 00:28:28 +0300

    bin search iter

--- base/src/ls/edu/BinarySearch.java
+++ base/src/ls/edu/BinarySearch.java
@@ -4,15 +4,31 @@ import java.util.Arrays;
 
 public class BinarySearch {
 
-    public int search(int[] sorted, int v) {
-        return -1;
-    }
-
     public static void main(String[] args) {
         BinarySearch bs = new BinarySearch();
 
-        int[] a = new int[]{1,2,4,6,9};
+        int[] a = new int[]{1, 2, 4, 6, 8, 9, 11};
         Arrays.sort(a);
-        System.out.println(bs.search(a, 6));
+        System.out.println(bs.search(a, 4));
+        System.out.println(bs.search(a, 1));
+        System.out.println(bs.search(a, 11));
+        System.out.println(bs.search(a, 9));
+        System.out.println(bs.search(a, 3));
+    }
+
+    public int search(int[] s, int v) {
+        int i = 0;
+        int j = s.length;
+
+        while (true) {
+            if(i > j) return -1;
+
+            int mid = (i + j) / 2;
+
+            if(v == s[mid]) return mid;
+            else if(v > s[mid]) {i = mid + 1;}
+            else {j = mid - 1;};
+
+        }
     }
 }
